c062a311f4924089b0b35605f8cc69950e495661
Alexis Diaz <alexdan1993@gmail.com>
2023-06-20 00:45:25 -0300

    Adds next-auth, supabase, nodemailer. Setting up Auth config. Adds buttons to play with auth views.

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth, { AuthOptions } from 'next-auth'
+import EmailProvider from 'next-auth/providers/email'
+import { SupabaseAdapter } from '@auth/supabase-adapter'
+
+export const authOptions = {
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
+  }),
+} as AuthOptions
+
+export default NextAuth(authOptions)
